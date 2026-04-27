be57b37fa6994c02b3ffc6b0f1010952dbacb84c
Melaniecrzx <melanie.crzx@outlook.fr>
2026-04-27 22:34:23 +0200

    feat: adjust Transaction schema

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -3,14 +3,19 @@ const mongoose = require('mongoose');
 const transactionSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
+    trim: true,
     ref: 'User',
   },
   avatar: {
     type: String,
+    trim: true,
+
     required: [true, 'A transaction must have an avatar'],
   },
   name: {
     type: String,
+    trim: true,
+
     required: [true, 'A transaction must have a name'],
   },
   date: {
@@ -19,6 +24,7 @@ const transactionSchema = new mongoose.Schema({
   },
   category: {
     type: String,
+    trim: true,
   },
   amount: {
     type: Number,
@@ -26,7 +32,7 @@ const transactionSchema = new mongoose.Schema({
   },
   recurring: {
     type: Boolean,
-    required: [true, 'A transaction must have a recurring'],
+    default: false,
   },
 });
 
