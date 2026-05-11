3a77da9d7d4f50d531f3e3970d4cc7ab0a531754
Melaniecrzx <melanie.crzx@outlook.fr>
2026-05-11 10:52:57 +0200

    feat: add global error handler catchAsync and AppError class

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const app = express();
-
-app.use(cors({ origin: 'http://localhost:5173' }));
+const AppError = require('./utils/appError.js');
+const globalErrorHandler = require('./controllers/errorController.js');
 
 const userRouter = require('./routes/userRoutes.js');
 const authRouter = require('./routes/authRoutes.js');
 const transactionRouter = require('./routes/transactionRoutes.js');
+
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 // 1) Middlewares
 app.use(morgan('dev'));
@@ -23,6 +26,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/transactions', transactionRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
+});
+
+app.use(globalErrorHandler);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: ' API is running', app: 'Finance' });
